8193b42b3aa6edde71a1cd741e56d45880f1d70a
elayjo <sochok2024@gmail.com>
2022-02-09 18:19:14 +0200

    add swagger

--- src/main/java/md/lfai/controller/AutorizationController.java
+++ src/main/java/md/lfai/controller/AutorizationController.java
@@ -1,10 +1,12 @@
 package md.lfai.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(AutorizationController.ULR_PREFIX)
+@Tag(name = "Модуль с авторизацией")
 public class AutorizationController {
     public static   final String ULR_PREFIX="/auth";
 }
--- src/main/java/md/lfai/controller/UserController.java
+++ src/main/java/md/lfai/controller/UserController.java
@@ -1,11 +1,24 @@
 package md.lfai.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(UserController.ULR_PREFIX)
+@Tag(name = "Работа на дпользователями")
 public class UserController {
 public static   final String ULR_PREFIX="/user";
+private static   final String LIST="/list";
+@GetMapping(LIST)
+//@Tag(name = "Список пользователей")
+public List<Object> list()
+{
+    return null;
+}
+
 
 }
