690492ac5ff85de6196101809760052b75cc88ca
kob <sochok2024@gmail.com>
2022-02-10 14:45:30 +0200

    add vue modules part I

---
+++ webpack.common.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+
+    entry: path.join(__dirname, 'src', 'main', 'resources',  'js', 'main.js'),
+
+
+    module: {
+        rules: [
+            {
+                test: /\.s(c|a)ss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        // Requires sass-loader@^7.0.0
+                        options: {
+                            implementation: require('sass'),
+                            indentedSyntax: true // optional
+                        },
+                        // Requires >= sass-loader@^8.0.0
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+                                indentedSyntax: true // optional
+                            },
+                        },
+                    },
+                ],
+            },
+
+            {
+                test: /\.svg$/,
+                use: [
+                    'babel-loader',
+                    'vue-svg-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+
+            {
+                test: /\.png$/,
+                loader: ['url-loader'],
+            },{
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader'],
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        modules: [
+            path.join(__dirname, 'src', 'main', 'resources',  'js'),
+            path.join(__dirname, 'node_modules'),
+        ],
+    }
+}
